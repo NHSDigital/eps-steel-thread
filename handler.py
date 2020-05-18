1cd8fef8216cbb4063608a53848a6f5cb8f40540
Matt Jarvis <matt.jarvis@gmail.com>
2020-05-18 12:32:49 +0100

    Removed CORS header as this is added by the Apigee proxy.

--- handler.py
+++ handler.py
@@ -1,8 +1,5 @@
 def main_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello, world",
-        "headers": {
-            "Access-Control-Allow-Origin": "*"
-        }
+        "body": "Hello, world"
     }
