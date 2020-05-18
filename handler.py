a981948f0b464ec869bfe3a20f1e2365c1f6e631
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-05-18 15:37:42 +0100

    added Flask and servserless-wsgi to pip

--- handler.py
+++ handler.py
@@ -1,5 +1,20 @@
+from flask import Flask
+import serverless_wsgi
+
 def main_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello, world"
-    }
+    app = Flask(__name__)
+    request = serverless_wsgi.handle_request(app, event, context)
+
+    @app.route('/', methods=['GET'])
+    def hello_world():
+        return {
+            "statusCode": 200,
+            "body": "Hello, world"
+        }
+
+    @app.route('/sign', methods=['POST'])
+    def sign():
+        return {
+            "statusCode": 200,
+            "body": request.body
+        }
