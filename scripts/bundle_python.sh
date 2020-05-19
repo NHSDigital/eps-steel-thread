b8afd11483e27e3a7e9efb44bf290ff6ae24540d
Richard Crawley <richard.crawley1@nhs.net>
2020-05-19 11:07:43 +0100

    Fix deployment script

--- scripts/bundle_python.sh
+++ scripts/bundle_python.sh
@@ -1,12 +1,12 @@
+# Make sure pipenv is good to go
+echo "Do fresh install to make sure everything is there"
+python3 -m pipenv install
+
 VENV=$(python3 -m pipenv --venv)
 ls $VENV/lib
 SITE_PACKAGES=$VENV/lib/python3.6/site-packages
 echo "Library Location: $SITE_PACKAGES"
 DIR=$(pwd)
-
-# Make sure pipenv is good to go
-echo "Do fresh install to make sure everything is there"
-python3 -m pipenv install
 
 cd $SITE_PACKAGES
 zip -r9 $DIR/handler.zip *
