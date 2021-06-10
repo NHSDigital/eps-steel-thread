9c1dfe8694e0b3027380064f954c609f7a69a290
Alex McCormick <amccmick1@googlemail.com>
2021-06-10 17:12:34 +0100

    Add placeholder logic switches for RSS support

--- api.py
+++ api.py
@@ -6,10 +6,14 @@ import json
 from jwt import JWT, jwk_from_pem
 
 DEMO_APP_CLIENT_ID = os.environ["DEMO_APP_CLIENT_ID"]
-DEMO_APP_PRIVATE_KEY = os.environ["DEMO_APP_PRIVATE_KEY"]
 DEMO_APP_KEY_ID = os.environ["DEMO_APP_KEY_ID"]
 EPS_BASE_PATH = os.environ["EPS_BASE_PATH"]
 SIGNING_BASE_PATH = os.environ["SIGNING_BASE_PATH"]
+
+# patch for RSS support whilst requirements for local signing and RSS are different
+# todo: use same private key for remote and local signing
+DEMO_APP_LOCAL_SIGNING_PRIVATE_KEY = os.environ["DEMO_APP_PRIVATE_KEY"]
+DEMO_APP_REMOTE_SIGNING_PRIVATE_KEY = ""
 
 
 def make_eps_api_prepare_request(access_token, body):
@@ -41,23 +45,31 @@ def make_eps_api_request(path, access_token, body):
 
 def make_sign_api_signature_upload_request(auth_method, access_token, digest, algorithm):
     jwt_client = JWT()
-    signing_key = jwk_from_pem(DEMO_APP_PRIVATE_KEY.encode("utf-8"))
-    jwt_request = jwt_client.encode(
-        {
-            'sub': DEMO_APP_CLIENT_ID,
-            'iss': DEMO_APP_CLIENT_ID,
-            'aud': SIGNING_BASE_PATH,
-            'iat': time.time(),
-            'exp': time.time() + 600,
-            'payload': digest,
-            'algorithm': algorithm
-        },
-        signing_key,
-        alg ="RS512",
-        optional_headers = {
-            'kid': DEMO_APP_KEY_ID
-        }
-    )
+
+    # patch for RSS support whilst requirements for local signing and RSS are different
+    # todo: remove this logic once they are aligned
+    if auth_method == "cis2":
+        signing_key = jwk_from_pem(DEMO_APP_LOCAL_SIGNING_PRIVATE_KEY.encode("utf-8"))
+        jwt_request = jwt_client.encode(
+            {
+                'sub': DEMO_APP_CLIENT_ID,
+                'iss': DEMO_APP_CLIENT_ID,
+                'aud': SIGNING_BASE_PATH,
+                'iat': time.time(),
+                'exp': time.time() + 600,
+                'payload': digest,
+                'algorithm': algorithm
+            },
+            signing_key,
+            alg ="RS512",
+            optional_headers = {
+                'kid': DEMO_APP_KEY_ID
+            }
+        )
+    else: # always 'simulated'
+        signing_key = jwk_from_pem(DEMO_APP_REMOTE_SIGNING_PRIVATE_KEY.encode("utf-8"))
+        # todo: build jwt for remote signing
+
     signing_base_url = get_signing_base_path(auth_method)
     return httpx.post(
         f"{signing_base_url}/signaturerequest",
