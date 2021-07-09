819dd04213da983804daf0a96d09a51542332f69
Dan Jones <dan.jones@infinityworks.com>
2021-07-09 16:33:36 +0100

    remove publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             filename: "templates/client.html",
-            publicPath: "~",
+            publicPath: "",
             inject: "head",
             scriptLoading: "blocking"
 
