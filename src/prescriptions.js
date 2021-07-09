e6ec59c2f0d80c745417d2831eed9760734e8480
Dan Jones <dan.jones@infinityworks.com>
2021-07-09 11:33:49 +0100

    make everything async but better

--- src/prescriptions.js
+++ src/prescriptions.js
@@ -10,7 +10,7 @@ async function getPrescription(path) {
     {
       method: "GET"
     })
-  return response.json();
+  return await response.json();
   // var xmlHttp = new XMLHttpRequest();
   // // use string concatenation for IE compatibility, interpolation is not supported
   // xmlHttp.open("GET", epsModelsUrl + "/" + path, false);
@@ -22,31 +22,31 @@ async function getPrescription(path) {
 /**
  * Examples
  */
+const PRIMARY_CARE_ACUTE_NOMINATED = getPrescription(
+  "primary-care/acute/nominated-pharmacy/medical-prescriber/1-Prepare-Request-200_OK.json"
+)
+const PRIMARY_CARE_REPEAT_PRESCRIBING_NOMINATED = getPrescription(
+  "primary-care/repeat-prescribing/1-Prepare-Request-200_OK.json"
+)
+const SECONDARY_CARE_COMMUNITY_ACUTE_NOMINATED = getPrescription(
+  "secondary-care/community/acute/nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
+)
+const SECONDARY_CARE_REPEAT_DISPENSING_NOMINATED = getPrescription(
+  "secondary-care/community/repeat-dispensing/nominated-pharmacy/clinical-practitioner/multiple-medication-requests/prescriber-endorsed/1-Prepare-Request-200_OK.json"
+)
+const SECONDARY_CARE_COMMUNITY_ACUTE_NON_NOMINATED = getPrescription(
+  "secondary-care/community/acute/no-nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
+)
+const HOMECARE_ACUTE_NOMINATED = getPrescription(
+  "secondary-care/homecare/acute/nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
+)
 export default {
-  PRIMARY_CARE_ACUTE_NOMINATED: await getPrescription(
-    "primary-care/acute/nominated-pharmacy/medical-prescriber/1-Prepare-Request-200_OK.json"
-  ),
-  PRIMARY_CARE_REPEAT_PRESCRIBING_NOMINATED: await getPrescription(
-    "primary-care/repeat-prescribing/1-Prepare-Request-200_OK.json"
-  ),
-  SECONDARY_CARE_COMMUNITY_ACUTE_NOMINATED: await getPrescription(
-    "secondary-care/community/acute/nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
-  ),
-  SECONDARY_CARE_COMMUNITY_ACUTE_NON_NOMINATED: await getPrescription(
-    "secondary-care/community/acute/no-nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
-  ),
-  SECONDARY_CARE_REPEAT_DISPENSING_NOMINATED: await getPrescription(
-    "secondary-care/community/repeat-dispensing/nominated-pharmacy/clinical-practitioner/multiple-medication-requests/prescriber-endorsed/1-Prepare-Request-200_OK.json"
-  ),
-  SECONDARY_CARE_COMMUNITY_ACUTE_NON_NOMINATED: await getPrescription(
-    "secondary-care/community/acute/no-nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
-  ),
-  SECONDARY_CARE_REPEAT_DISPENSING_NOMINATED: await getPrescription(
-    "secondary-care/community/repeat-dispensing/nominated-pharmacy/clinical-practitioner/multiple-medication-requests/prescriber-endorsed/1-Prepare-Request-200_OK.json"
-  ),
-  HOMECARE_ACUTE_NOMINATED: await getPrescription(
-    "secondary-care/homecare/acute/nominated-pharmacy/clinical-practitioner/1-Prepare-Request-200_OK.json"
-  )
+  PRIMARY_CARE_ACUTE_NOMINATED,
+  PRIMARY_CARE_REPEAT_PRESCRIBING_NOMINATED,
+  SECONDARY_CARE_COMMUNITY_ACUTE_NOMINATED,
+  SECONDARY_CARE_COMMUNITY_ACUTE_NON_NOMINATED,
+  SECONDARY_CARE_REPEAT_DISPENSING_NOMINATED,
+  HOMECARE_ACUTE_NOMINATED
 }
 //let PRIMARY_CARE_REPEAT_DISPENSING_NOMINATED = getPrescription("primary-care/repeat-dispensing/nominated-pharmacy/medical-prescriber/author/gmc/responsible-party/medication-list/din/1-Prepare-Request-200_OK.json")
 //let SECONDARY_CARE_REPEAT_PRESCRIBING_NOMINATED = getPrescription("todo")
